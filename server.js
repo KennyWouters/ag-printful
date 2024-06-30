b18e6fa9eb47ea07d43fe1bc4bac3249f89f7ea3
KennyWouters <kenny.wouters2@gmail.com>
2024-06-30 22:44:20 +0200

    server created in server.js, building w/ npm start in root folder, running on localhost : 3001

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // 3. Defining route to manage http requests
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Server up and running guyz!/ Have a nice day and Happy coding! ;). K');
 });
 
 // 4. Starting server on a specific port
